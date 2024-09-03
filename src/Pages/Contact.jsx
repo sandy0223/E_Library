37923f1a7231d9034c389375f4d177c1c6741bfb
Partha Sarathi Ratha <parthratha@gmail.com>
2024-09-03 23:18:10 +0530

    contact us

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="center flex justify-center  flex-col items-center m-8">
+        <div className="text-5xl font-normal flex justify-center  text-blue-700 ml-6 font-mono">
+          Contact us
+        </div>
+        <div className="text-gray-800 border-2  border-black rounded-full  w-60 ml-6 pl-3 mt-6 ">
+          <input type="text" id="name" placeholder="Name" />
+        </div>
+        <div className="text-gray-800 border-2 border-black rounded-full w-60 ml-6 pl-3 mt-3">
+          <input type="email" id="email" placeholder="Email" />
+        </div>
+        <div className="text-gray-800 border-2 border-black rounded-lg w-60 ml-6 pl-3 mt-3 p-8">
+          <input type="text" id="text" placeholder="Message" />
+        </div>
+        <div className="text-white border-blue-800  bg-blue-600 rounded-full text-center w-60 ml-6 pl-3 p-1 mt-3">
+          <button>Send Message</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
